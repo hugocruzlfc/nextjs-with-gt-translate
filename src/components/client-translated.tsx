5e0657bb39ea62e36e0d91298764b1e10f75d6fb
Hugo Cruz <hugocruzlfc@gmail.com>
2025-03-05 11:19:14 +0100

    feat: add gt setup

---
+++ src/components/client-translated.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { T } from "gt-next";
+
+export default function ClientTranslated() {
+  return (
+    <T>
+      <h2>
+        General Translation is an entire i18n stack that allows you to ship
+        multilingual apps quickly and easily.
+      </h2>
+      <h2>General Translation includes the following:</h2>
+      <p>Open-source developer libraries for React and Next.js</p>
+      <p>An AI translation provider</p>
+      <p>A complete infrastructure package for serving translation content</p>
+    </T>
+  );
+}
